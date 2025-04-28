9a29532e79b21b8a1dad43fdd063b7d197abcd72
Xanadar0 <61867465+Xanadar0@users.noreply.github.com>
2025-04-28 19:02:38 +0300

    create all

---
+++ js/switcher.js
@@ -0,0 +1,24 @@
+function setTheme(themeName) {
+    localStorage.setItem('theme', themeName);
+    document.documentElement.classList = themeName;
+}
+
+function toggleTheme() {
+    if (localStorage.getItem('theme') === 'theme-dark') {
+        setTheme('theme-light');
+    } else {
+        setTheme('theme-dark');
+    }
+}
+
+
+(function () {
+    if (localStorage.getItem('theme') === 'theme-dark') {
+        setTheme('theme-dark');
+        document.getElementById('slider').checked = false;
+    } else {
+        setTheme('theme-light');
+        document.getElementById('slide').checked = true;
+
+    }
+})();
